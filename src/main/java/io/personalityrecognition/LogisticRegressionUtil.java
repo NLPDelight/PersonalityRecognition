7b5371035a0e5296aa839a0493f60be1195d04a5
tom <tomwang2011@gmail.com>
2016-05-28 09:14:53 -0700

    Logistic Regression Test

---
+++ src/main/java/io/personalityrecognition/LogisticRegressionUtil.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.personalityrecognition;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.apache.mahout.classifier.evaluation.Auc;
+import org.apache.mahout.classifier.sgd.L1;
+import org.apache.mahout.classifier.sgd.OnlineLogisticRegression;
+import org.apache.mahout.math.DenseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.vectorizer.encoders.ConstantValueEncoder;
+import org.apache.mahout.vectorizer.encoders.StaticWordValueEncoder;
+
+/**
+ *
+ * @author tom
+ */
+public class LogisticRegressionUtil {
+	public double test(Map<String, Double> testMap, OnlineLogisticRegression olr) {
+
+		testModel(testMap, olr);
+
+		return 0;
+	}
+
+	public OnlineLogisticRegression testMethod(List<Observation> trainingData) {
+		return train(trainingData);
+	}
+
+	public List<Observation> parseInput(Map<String, Double> map, Map<String, Double> mapB) {
+		List<Observation> result = new ArrayList<>();
+
+		for (Entry<String, Double> entry : map.entrySet()) {
+			result.add(new Observation(1, entry.getKey(), entry.getValue()));
+		}
+
+		for (Entry<String, Double> entry : mapB.entrySet()) {
+			result.add(new Observation(0, entry.getKey(), entry.getValue()));
+		}
+
+		return result;
+	}
+
+	public List<Observation> parseTest(Map<String, Double> list) {
+		List<Observation> result = new ArrayList<>();
+
+		for (Entry<String, Double> s : list.entrySet()) {
+			result.add(new Observation(1, s.getKey(), s.getValue()));
+		}
+
+		return result;
+	}
+
+	public OnlineLogisticRegression train(List<Observation> trainData) {
+		OnlineLogisticRegression olr = new OnlineLogisticRegression(2, 3,
+			new L1());
+		// Train the model using 30 passes
+		for (int pass = 0; pass < 30; pass++) {
+			for (Observation observation : trainData) {
+				olr.train(observation.getActual(), observation.getVector());
+			}
+			// Every 10 passes check the accuracy of the trained model
+			if (pass % 1 == 0) {
+				Auc eval = new Auc(0.5);
+				for (Observation observation : trainData) {
+					eval.add(observation.getActual(),
+						olr.classifyScalar(observation.getVector()));
+				}
+//				System.out.format(
+//					"Pass: %2d, Learning rate: %2.4f, Accuracy: %2.4f\n",
+//					pass, olr.currentLearningRate(), eval.auc());
+			}
+		}
+		return olr;
+	}
+
+	public double testModel(Map<String, Double> testMap, OnlineLogisticRegression olr) {
+		List<Observation> testData = parseTest(testMap);
+
+		List<Double> results = new ArrayList<>();
+
+		double sum = 0;
+		for (Observation o : testData) {
+			Vector result = olr.classifyFull(o.getVector());
+
+			results.add(result.get(1));
+
+			sum += result.get(1);
+//			System.out.println("------------- Testing -------------");
+//			System.out.format("Probability of not fraud (0) = %.3f\n",
+//			result.get(0));
+//			System.out.format("Probability of fraud (1)     = %.3f\n",
+//			result.get(1));
+			}
+		return sum/testData.size();
+		}
+
+	class Observation {
+
+		private DenseVector vector = new DenseVector(3);
+		private int actual;
+
+		public Observation(int actual, String gram, double value) {
+			ConstantValueEncoder interceptEncoder = new ConstantValueEncoder(
+				"intercept");
+			StaticWordValueEncoder encoder = new StaticWordValueEncoder(
+				"feature");
+
+			interceptEncoder.addToVector("1", vector);
+			encoder.addToVector(gram, vector);
+			vector.set(0, value);
+			this.actual = actual;
+		}
+
+		public Vector getVector() {
+			return vector;
+		}
+
+		public int getActual() {
+			return actual;
+		}
+	}
+}
