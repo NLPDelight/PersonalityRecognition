b7b89872cc51d788b735c737052867bc687b2b61
tom <tomwang2011@gmail.com>
2016-05-29 22:42:30 -0700

    Using java 7 syntax as the testing machine doesn't have java 8

--- src/main/java/io/personalityrecognition/util/TypeCounter.java
+++ src/main/java/io/personalityrecognition/util/TypeCounter.java
@@ -2,6 +2,7 @@ package io.personalityrecognition.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -16,43 +17,43 @@ import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
 
 public class TypeCounter {
-	
+
 	private Tokenizer tokenizer;
 	private TokenizerModel model;
 	private HashSet<String> stopwords;
-	
+
 	public TypeCounter() {
 		tokenizer = SimpleTokenizer.INSTANCE;
 		stopwords = new HashSet<String>();
 	}
-	
+
 	public TypeCounter(String modelFileName) throws IOException {
 		model = new TokenizerModel(new File(modelFileName));
 		tokenizer = new TokenizerME(model);
 		stopwords = new HashSet<String>();
 	}
-	
+
 	public TypeCounter(String modelFileName, String stopwordFileName) throws IOException {
 		model = new TokenizerModel(new File(modelFileName));
 		tokenizer = new TokenizerME(model);
 		stopwords = fileLinesToSet(stopwordFileName);
 	}
-	
+
 	private HashSet<String> fileLinesToSet(String stopwordFileName) throws IOException {
 		Path fileLocation = new File(stopwordFileName).toPath();
-		List<String> stopwords = Files.readAllLines(fileLocation);
+		List<String> stopwords = Files.readAllLines(fileLocation, StandardCharsets.UTF_8);
 		return new HashSet<String>(stopwords);
 	}
-	
+
 	public String[] tokenize(String line) {
 		return tokenizer.tokenize(line);
 	}
-	
+
 	public Map<String, Integer> countTypesInSet(String line, Set<String> words) {
 		String[] tokens = tokenizer.tokenize(line);
 		return countTypesInSet(tokens, words);
 	}
-	
+
 	private Map<String, Integer> countTypesInSet(String[] tokens, Set<String> lexicon) {
 		Map<String, Integer> counts = new HashMap<String, Integer>();
 		for(int i = 0; i < tokens.length; i++) {
@@ -62,12 +63,12 @@ public class TypeCounter {
 		}
 		return counts;
 	}
-	
+
 	public Map<String, Integer> countTypes(String line) {
 		String[] tokens = tokenizer.tokenize(line);
 		return countTypes(tokens);
 	}
-	
+
 	private Map<String, Integer> countTypes(String[] tokens) {
 		Map<String, Integer> counts = new HashMap<String, Integer>();
 		for(int i = 0; i < tokens.length; i++) {
@@ -77,7 +78,7 @@ public class TypeCounter {
 		}
 		return counts;
 	}
-	
+
 	private void addTokenToCounts(String token, Map<String, Integer> counts) {
 		if(counts.containsKey(token))
 			counts.put(token, counts.get(token) + 1);
